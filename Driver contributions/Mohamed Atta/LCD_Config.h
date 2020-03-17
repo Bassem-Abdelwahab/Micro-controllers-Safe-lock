159c8263dd96aa083f5c001bdb2cae19a1607ad5
mohamed07a <58663977+mohamed07a@users.noreply.github.com>
2020-03-17 23:14:07 +0200

    Add files via upload

---
+++ Driver contributions/Mohamed Atta/LCD_Config.h
@@ -0,0 +1,19 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Feb 29, 2020
+ *      Author: Mohamed
+ */
+#include "GPIO_Config.h"
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define LCD_Data_Port     PortB
+#define LCD_Control_Port  PortD
+#define RS Pin0
+#define RW Pin1
+#define E  Pin2
+
+
+#endif /* LCD_CONFIG_H_ */
